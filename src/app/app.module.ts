2683a4fb91336bc2437db20689f1058a929593ab
bartech-g <bartech.js@gmail.com>
2024-01-28 11:18:17 +0100

    create auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { UserModule } from './user/user.module';
 import {AngularFireModule} from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import {AngularFireAuthModule}  from '@angular/fire/compat/auth';
-
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule, AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule, AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule, AngularFirestoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/services/auth.service';
 import { compileNgModule } from '@angular/compiler';
-
+import IUser from 'src/app/models/user.model';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  constructor(private auth: AngularFireAuth) {
+  constructor(private auth: AuthService) {
 
   }
   inSubmission = false;
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.email]);
-  age = new FormControl('', [Validators.min(18)]);
+  age = new FormControl<number | null>(null, [Validators.min(18)]);
   password = new FormControl('', [
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
@@ -37,12 +37,9 @@ export class RegisterComponent {
     this.showAlert = true;
     this.inSubmission = true;
     // Firebase Auth
-    const { email, password } = this.registerForm.value
+
     try {
-      const userCred = await this.auth.createUserWithEmailAndPassword(
-        email as string, password as string
-      )
-      console.log(userCred)
+      await this.auth.createUser(this.registerForm.value as IUser)
     } catch (e) {
       console.log(e)
       this.alertMsg = "Error, try again..."
